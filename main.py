1fe6ec562e0312111070c8210ed226ca0d71f4cb
ZiadSheriif <zsherif308@gmail.com>
2024-04-03 03:28:38 +0200

    add parentheses and hyphens preprocessing

--- main.py
+++ main.py
@@ -9,5 +9,8 @@ from src.regex_validator import RegexValidator
 if __name__ == "__main__":
     regex_one = RegexValidator(r"(\d+)")
     regex_two = RegexValidator(r"[A-Za-z")
+    regex_three = RegexValidator(r"[a-zA-Z]")
     print(regex_one.validate())
     print(regex_two.validate())
+    print(regex_three.validate())
+    regex_three.post_validate()
--- src/regex_validator.py
+++ src/regex_validator.py
@@ -4,6 +4,7 @@ import re
 class RegexValidator:
     def __init__(self, regex):
         self.regex = regex
+        self.postfix = None
 
     def validate(self):
         try:
@@ -11,3 +12,63 @@ class RegexValidator:
             return True
         except re.error:
             return False
+
+    def post_validate(self):
+        # we will order regular expressions syntax is listed as in the documentation
+        # supported regex syntax
+        operators = {
+            "|": 1,
+            ".": 2,
+            "?": 3,
+            "*": 3,
+            "+": 3,
+            "^": 4,
+            "$": 4,
+            "(": 5,
+            ")": 5,
+        }
+        regex = self.regex
+
+        # Check if the regular expression contains any character classes (denoted by square brackets).
+        # If a character class is found, the function converts it to an "alternation" #!`() between the characters inside the class`.
+        # as example: [xyz] => (x|y|z) and [0-9] will be converted to (0|1|2|3|4|5|6|7|8|9) and so on.
+
+        for i in range(len(regex)):
+            operator = regex[i]
+            if operator == "[":
+                j = i + 1
+                while regex[j] != "]":
+                    if regex[j].isalnum() and regex[j + 1].isalnum():
+                        regex = regex[: j + 1] + "|" + regex[j + 1 :]
+                    j += 1
+
+        # then , replace the character class with the new alternation
+        regex = regex.replace("[", "(").replace("]", ")")
+        print(regex)
+
+        ############################################################
+        ############################################################
+
+        # replace hyphen with operator "|" to separate the range of characters in the character class
+        # as example: [0-9] will be converted to (0|1|2|3|4|5|6|7|8|9) and so on.
+        # as example: [a-z] will be converted to (a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z) and so on.
+        regex_copy = regex[:]
+        print(regex_copy)
+        hyphens_count = regex_copy.count("-")
+        print("hyphens_count: ", hyphens_count)
+        print("regex_copy: ", len(regex_copy))
+        for j in range(len(regex_copy)):
+                operator = regex_copy[j]
+                # if (a-z) ==> (a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z)
+                if operator == "-":
+                    temp = ""
+                    end = ord(regex[j + 1])
+                    start = ord(regex[j - 1])
+                    for z in range(int(end - start)):
+                        temp += "|"
+                        char = chr(start + z + 1)
+                        print("char: ", char)
+                        temp += char
+                    regex = regex[0:j] + temp + regex[j + 2 :]
+                    # break
+        print("regex after replacing hyphens with alternation: ", regex)
