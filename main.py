abb1ec4486185f3a0c2c1c7e927567a8269f379c
ZiadSheriif <zsherif308@gmail.com>
2024-04-04 19:29:22 +0200

    Update integration with main

--- main.py
+++ main.py
@@ -3,12 +3,21 @@
 ############################
 
 from src.regex_validator import RegexValidator
+from src.nfa import NFA
 
 if __name__ == "__main__":
 
     print("##########  VALIDATION  #############")
     regex_one = RegexValidator(r"ab(c|d)")
-    regex_three = RegexValidator(r"(A+B)*(C+D)")
+    regex_three = RegexValidator(r"ab*")
 
     print("#######################")
     postfix_regex = regex_three.post_validate()
+    nfa = NFA(postfix=postfix_regex)
+    print("NFA ",nfa.to_graph())
+    print("#######################")
+    nfa.visualize(name="output/nfa.gv",view=False)
+    
+    
+    
+    
