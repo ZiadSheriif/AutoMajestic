624c2df0b1e49a2a8dc8b7759dfa18132d965c72
mohamedromee12 <mohamedromee12@gmail.com>
2024-04-12 16:18:48 +0200

    fix show error in frontend

--- dfa-frontend/src/view/pages/Home/Home.tsx
+++ dfa-frontend/src/view/pages/Home/Home.tsx
@@ -14,8 +14,9 @@ import CircularProgress from "@mui/material/CircularProgress";
 
 const Home = () => {
   const [regex, setRegex] = useState("");
-  const [type, setType] = useState("NFA");
+  const [type, setType] = useState("");
   const [image, setImage] = useState();
+  const [enter, setEnter] = useState(false);
 
   const {
     nfa,
@@ -39,39 +40,56 @@ const Home = () => {
   } = useCompile();
 
   useEffect(() => {
-    if (type == "NFA") {
-      if (data_nfa) {
-        setImage(data_nfa);
-      }
-    } else if (type == "DFA") {
-      if (data_dfa) {
-        setImage(data_dfa);
-      }
-    } else if (type == "MIN-DFA") {
-      if (data_min_dfa) {
-        setImage(data_min_dfa);
+    if (!isLoading_nfa && !isLoading_dfa && !isLoading_min_dfa) {
+      if (type == "NFA") {
+        if (data_nfa) {
+          setImage(data_nfa);
+        } else if (isError_nfa) {
+          toast.error("This Regex is invalid");
+        }
+      } else if (type == "DFA") {
+        if (data_dfa) {
+          setImage(data_dfa);
+        } else if (isError_dfa) {
+          toast.error("This Regex is invalid");
+        }
+      } else if (type == "MIN-DFA") {
+        if (data_min_dfa) {
+          setImage(data_min_dfa);
+        } else if (isError_min_dfa) {
+          toast.error("This Regex is invalid");
+        }
       }
     }
-  }, [data_nfa, data_dfa, data_min_dfa]);
-
-  useEffect(() => {
-    if (isError_nfa) {
-      toast.error("This Regex is invalid");
-    }
-  }, [isError_nfa]);
+  }, [
+    data_nfa,
+    data_dfa,
+    data_min_dfa,
+    type,
+    enter,
+    isLoading_nfa,
+    isLoading_dfa,
+    isLoading_min_dfa,
+    isError_nfa,
+    isError_dfa,
+    isError_min_dfa,
+  ]);
 
   const handleGetNfa = () => {
     setType("NFA");
+    setEnter(!enter);
     nfa(regex);
   };
 
   const handleGetDfa = () => {
     setType("DFA");
+    setEnter(!enter);
     dfa(regex);
   };
 
   const handleGetMinDfa = () => {
     setType("MIN-DFA");
+    setEnter(!enter);
     minDfa(regex);
   };
 
@@ -138,7 +156,11 @@ const Home = () => {
             ) : (
               <>
                 <Typography
-                  sx={{ fontSize: "1.6rem", marginBottom: "5px", color: "white" }}
+                  sx={{
+                    fontSize: "1.6rem",
+                    marginBottom: "5px",
+                    color: "white",
+                  }}
                 >
                   {type}
                 </Typography>
